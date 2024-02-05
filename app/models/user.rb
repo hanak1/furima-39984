ce756194c52d394b9fe9bdb09bf2e910d987ae05
hanak1 <k587mama@gmail.com>
2024-02-05 17:50:04 +0900

    再度直し　validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nick_name, presence: true
-  validates :password_confirmation, presence: true, length: { minimum: 6 },
+  validates :password, presence: true, length: { minimum: 6 },
                                     format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
