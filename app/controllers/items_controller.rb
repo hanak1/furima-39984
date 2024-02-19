6e8edfa181803401ac806ce86f991dec100bc0c2
hanak1 <k587mama@gmail.com>
2024-02-19 19:53:25 +0900

    商品詳細ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,15 +19,15 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  #   item = Item.find(params[:id])
-  #   item.destroy
-  #   redirect_to root_path
-  # end
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
 
-  # def show
-  #    @item = Item.find(params[:id])
-  # end
+  def show
+    @item = Item.find(params[:id])
+  end
 
 private
 
