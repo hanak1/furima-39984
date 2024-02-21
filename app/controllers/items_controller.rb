be4c68526bca08bf47134c292a5c9bc3a486013a
hanak1 <k587mama@gmail.com>
2024-02-21 16:55:30 +0900

    エラーハンドリングができる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,10 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-       redirect_to item_path(params[:id])
+      redirect_to item_path(params[:id])
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   # def destroy
@@ -36,7 +39,7 @@ class ItemsController < ApplicationController
   def show
   end
 
-private
+  private
 
   def item_params
     params.require(:item).permit(:product_name, :product_detail, :product_condition_id, :shipping_fee_id, :region_of_shipping_origin_id, :estimated_delivery_date_id, :price, :image, :category_id).merge(user_id: current_user.id)
@@ -45,4 +48,5 @@ private
   def set_item
     @item = Item.find(params[:id])
   end
+
 end
